ef34bdc722da737485821503e7ca46a8e02fb5ba
ProgProgP <progp@yahoo.com>
2023-11-10 22:04:39 +0100

    terraform fmt

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 locals {
-  vpc_name = "${var.env_name} ${var.vpc_name}"
+  vpc_name     = "${var.env_name} ${var.vpc_name}"
   cluster_name = "${var.cluster_name}-${var.env_name}"
 }
 
@@ -11,7 +11,7 @@ locals {
 resource "aws_vpc" "main" {
   cidr_block = var.main_vpc_cidr
   tags = {
-    "Name" = local.vpc_name,
+    "Name"                                        = local.vpc_name,
     "kubernetes.io/cluster/${local.cluster_name}" = "shared",
   }
 }
@@ -22,54 +22,54 @@ data "aws_availability_zones" "available" {
 }
 
 resource "aws_subnet" "public-subnet-a" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.public_subnet_a_cidr
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_a_cidr
   availability_zone = data.aws_availability_zones.available.names[0]
   tags = {
     "Name" = (
       "${local.vpc_name}-public-subnet-a"
     )
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/role/elb"                      = "1"
   }
 }
 
 resource "aws_subnet" "public-subnet-b" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.public_subnet_b_cidr
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_b_cidr
   availability_zone = data.aws_availability_zones.available.names[1]
   tags = {
     "Name" = (
       "${local.vpc_name}-public-subnet-b"
     )
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/role/elb"                      = "1"
   }
 }
 
 resource "aws_subnet" "private-subnet-a" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.private_subnet_a_cidr
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_a_cidr
   availability_zone = data.aws_availability_zones.available.names[0]
   tags = {
     "Name" = (
       "${local.vpc_name}-private-subnet-a"
     )
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/role/internal-elb"             = "1"
   }
 }
 
 resource "aws_subnet" "private-subnet-b" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.private_subnet_b_cidr
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_b_cidr
   availability_zone = data.aws_availability_zones.available.names[1]
   tags = {
     "Name" = (
       "${local.vpc_name}-private-subnet-b"
     )
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/role/internal-elb"             = "1"
   }
 }
 
@@ -93,12 +93,12 @@ resource "aws_route_table" "public-route" {
 }
 
 resource "aws_route_table_association" "public-a-association" {
-  subnet_id = aws_subnet.public-subnet-a.id
+  subnet_id      = aws_subnet.public-subnet-a.id
   route_table_id = aws_route_table.public-route.id
 }
 
 resource "aws_route_table_association" "public-b-association" {
-  subnet_id = aws_subnet.public-subnet-b.id
+  subnet_id      = aws_subnet.public-subnet-b.id
   route_table_id = aws_route_table.public-route.id
 }
 
@@ -118,8 +118,8 @@ resource "aws_eip" "nat-b" {
 
 resource "aws_nat_gateway" "nat-gw-a" {
   allocation_id = aws_eip.nat-a.id
-  subnet_id = aws_subnet.public-subnet-a.id
-  depends_on = [aws_internet_gateway.igw]
+  subnet_id     = aws_subnet.public-subnet-a.id
+  depends_on    = [aws_internet_gateway.igw]
   tags = {
     "Name" = "${local.vpc_name}-NAT-gw-a"
   }
@@ -127,8 +127,8 @@ resource "aws_nat_gateway" "nat-gw-a" {
 
 resource "aws_nat_gateway" "nat-gw-b" {
   allocation_id = aws_eip.nat-b.id
-  subnet_id = aws_subnet.public-subnet-b.id
-  depends_on = [aws_internet_gateway.igw]
+  subnet_id     = aws_subnet.public-subnet-b.id
+  depends_on    = [aws_internet_gateway.igw]
   tags = {
     "Name" = "${local.vpc_name}-NAT-gw-b"
   }
--- output.tf
+++ output.tf
@@ -4,10 +4,10 @@ output "vpc_id" {
 
 output "subnet_ids" {
   value = [
-	aws_subnet.public-subnet-a.id,
-	aws_subnet.public-subnet-b.id,
-	aws_subnet.private-subnet-a.id,
-	aws_subnet.private-subnet-b.id]
+    aws_subnet.public-subnet-a.id,
+    aws_subnet.public-subnet-b.id,
+    aws_subnet.private-subnet-a.id,
+  aws_subnet.private-subnet-b.id]
 }
 
 output "public_subnet_ids" {
